77f78c053b4a9a260e01c2b6763e66b46a8b21d9
takunology <m15025@g.metro-cit.ac.jp>
2019-12-30 19:50:09 +0900

    DLL読み込み

---
+++ DLLApp/DLLApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DLLApp
+{
+    class Program
+    {
+        [DllImport("DLLfile.dll")]
+        static extern int main(); //DLL内で定義された関数
+
+        static void Main(string[] args)
+        {
+            main(); //C言語のmain関数を実行する 
+        }
+    }
+}
---
+++ DLLApp/DLLfile/Source.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"dlldefine.h"
+
+int main() {
+	printf("‚±‚Ì•¶Žš‚ÍCŒ¾Œê‚Å•\Ž¦‚µ‚Ä‚¢‚Ü‚·!\n");
+	return 0;
+}
